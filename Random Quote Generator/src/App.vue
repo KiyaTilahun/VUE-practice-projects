26644557def634e795834c90ad67ce617260ff69
kiyatilahun <kiyatilahun0@gmail.com>
2025-01-09 17:35:38 +0300

    has made random quote

---
+++ Random Quote Generator/src/App.vue
@@ -0,0 +1,12 @@
+<template >
+<div class="w-screen">
+  <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center pb-6">Random <span class="text-blue-600 dark:text-blue-500">Quotes</span> </h1>
+<RandomQuote :class="text-center pb-6"/>
+</div>
+</template>
+<script setup>
+import RandomQuote from './components/RandomQuote.vue';
+</script>
+<style lang="">
+  
+</style>
---
+++ Random Quote Generator/src/components/RandomQuote.vue
@@ -0,0 +1,42 @@
+<template lang="">
+<div class="grid grid-cols-1 gap-4 " >
+ 
+  
+<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-screen">
+
+<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Todays Quote</h5>
+<p class="font-normal text-gray-700 dark:text-gray-400">"{{myquotes}}"</p>
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" v-on:click="getRandom">Get new Quote</button>
+
+</a>
+
+</div>
+
+</template>
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const myquotes = ref('--------');
+
+const getRandom = async () => {
+  try {
+    const min = 1;
+const max = 10;
+const randomNumber = Math.ceil(Math.random() * (max - min) + min);
+ 
+    
+    const response = await axios.get(`https://dummyjson.com/quotes/${randomNumber}`); // Replace with your API endpoint
+    myquotes.value=response.data.quote; 
+  } catch (error) {
+    console.error('Error fetching random data:', error);
+    throw error; 
+  }
+};
+
+
+
+</script>
+<style lang="">
+
+</style>
